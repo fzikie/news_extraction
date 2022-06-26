abdf219072ef31aaa0f5d489f15761e866d4f919
fzikie <96601983+fzikie@users.noreply.github.com>
2022-06-26 01:52:31 -0400

    Update web_scrap_addis_standard.py
    Signed-off-by: fzikie <96601983+fzikie@users.noreply.github.com>

--- web_scrap_addis_standard.py
+++ web_scrap_addis_standard.py
@@ -69,16 +69,4 @@ if __name__==__main__:
   extract_all_news()
 
 
-# # Function 2: Extract related words and phrases to poverty
-# def get_poverty_describing_words():
-#     url = 'https://relatedwords.org/relatedto/poverty'
-#     myurl = requests.get(url).text
-#     # print(myurl)
-#     soup = BeautifulSoup(myurl, 'lxml')
-#
-#     data = json.loads(soup.find('script', type="text/json").text)
-#     data_terms = data['terms']
-#     for term in data_terms:
-#         print(term['word'])
-
 
